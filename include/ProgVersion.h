aa321cf094b58cabdff096461fdbed2668b25946
LSC-Labs <60842788+LSC-Labs@users.noreply.github.com>
2025-10-24 13:20:28 +0200

    Version 1.0

--- include/ProgVersion.h
+++ include/ProgVersion.h
@@ -4,12 +4,12 @@
 // If you want to change the major/minor/patch version nummer, use the file data/prog_ver.json.
 // The build nummer will be incremented in this file by platform.io -> prebuild steps
 
-#define PROG_VERSION_NAME  "DewPoint-Monitor"
+#define PROG_VERSION_NAME  "DewPoint-Switch"
 
-#define PROG_VERSION_MAJOR 0
-#define PROG_VERSION_MINOR 9
+#define PROG_VERSION_MAJOR 1
+#define PROG_VERSION_MINOR 0
 #define PROG_VERSION_PATCH 0
-#define PROG_VERSION_BUILD 9
-#define PROG_VERSION       "0.9.0"
-#define PROG_VERSION_SHORT "0.9"
-#define PROG_VERSION_LONG  "0.9.0.9"
+#define PROG_VERSION_BUILD 0
+#define PROG_VERSION       "1.0.0"
+#define PROG_VERSION_SHORT "1.0"
+#define PROG_VERSION_LONG  "1.0.0.0"
